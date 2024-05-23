ad5c09cffd6f2c6d2281b4b9a5271c1b0e23d176
Alexander <kkruscherr@gmail.com>
2024-05-23 22:21:12 +0300

    Up to Sprint 9; Lesson 13

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,17 +1,11 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from typing import Any
+
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
 
 from .forms import BirthdayForm
 from .models import Birthday
-
-
-class BirthdayMixin:
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
-
-
-class BirthdayCreateView(BirthdayMixin, CreateView):
-    form_class = BirthdayForm
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
@@ -20,9 +14,27 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+class BirthdayCreateView(CreateView):
+    model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
