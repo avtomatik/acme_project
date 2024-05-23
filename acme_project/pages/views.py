fecda088e4eedce52261a5527b5f9ce95c81e7a7
Alexander <kkruscherr@gmail.com>
2024-05-23 22:47:20 +0300

    Up to Sprint 9; Lesson 14

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic import TemplateView
+
+from birthday.models import Birthday
 
 
-def homepage(request):
+class HomePage(TemplateView):
     template_name = 'pages/index.html'
-    return render(request, template_name)
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Birthday.objects.count()
+        return context
